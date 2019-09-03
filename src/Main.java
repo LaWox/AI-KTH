9e68cbab016d2fbb8bda244911384527f933d961
LaWox <woxler.platon@gmail.com>
2019-09-03 16:15:06 +0200

    last update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing the file passed in:");
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
     }
 }
