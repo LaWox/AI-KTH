5e829299d2d1ac7aa465d4eda7c0e9dc2a3e57c0
J Kangas <jussi.v.kangas@gmail.com>
2019-09-04 13:59:20 +0200

    nytt

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 
 public class Main {
 
+    static Float[] multiplication(String[] a, String[] b,){
+        int aRow = Integer.parseInt(a[0]);
+        int aCol = Integer.parseInt(a[1]);
+        int bRow = Integer.parseInt(b[0]);
+        int bCol = Integer.parseInt(b[1]);
+        Float[] output = new Float[aRow*bCol+2];
+        output[0] = (float) aRow;
+        output[1] = (float) bCol;
+
+        for (int i=0; i<aRow; i++ ){
+            int r
+
+        }
+    }
     public static void main(String[] args) {
         String[] aMatrix;
         String[] bMatrix;
@@ -15,7 +29,7 @@ public class Main {
         bMatrix = sc.nextLine().split(" ");
         pi = sc.nextLine().split(" ");
 
-        
+
 
     }
 }
