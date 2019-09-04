9dd95cfb01ee76111cf2553c502908bec911432b
J Kangas <jussi.v.kangas@gmail.com>
2019-09-04 16:49:52 +0200

    tete

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,36 @@ import java.util.ArrayList;
 public class Main {
 
 
-    static Float[] multiplication(Float[] a, Float[] b,){
-        int aRow = Integer.parseInt(a[0]);
-        int aCol = Integer.parseInt(a[1]);
-        int bRow = Integer.parseInt(b[0]);
-        int bCol = Integer.parseInt(b[1]);
-        Float[] output = new Float[aRow*bCol+2];
+    static float[] multiplication(float[][] a, float[][] b){
+        int aRow = a.length;
+        int aCol = a[0].length;
+        int bRow = b.length;
+        int bCol = b[0].length;
+        float[] output = new float[aRow*bCol+2];
         output[0] = (float) aRow;
         output[1] = (float) bCol;
+        int counter=2;
+        float sum=0;
 
         for (int i=0; i<aRow; i++ ){
-            output[i+2] = a[i+2
-
+            for( int j=0; j<bCol; j++){
+                for( int k=0; k<aCol; k++){
+                    sum += a[i][k]*b[k][j];
+                }
+                output[counter]=sum;
+                sum=0;
+                counter++;
+            }
         }
+
+        return output;
     }
 
-    static Float[][] createMatrix(Float[] a){
-        int aRow = Integer.parseInt(a[0]);
-        int aCol = Integer.parseInt(a[1]);
+    static float[][] createMatrix(float[] a){
+        int aRow = (int) a[0];
+        int aCol = (int) a[1];
         int counter= 2;
-        Float[][] matrix = new Float[aRow][aCol];
+        float[][] matrix = new float[aRow][aCol];
         for (int i= 0; i<aRow; i++){
             for(int j= 0; j<aCol; j++){
                 matrix[i][j]=a[counter];
@@ -40,9 +50,13 @@ public class Main {
         String[] bMatrixStr;
         String[] piStr;
 
-        Float[] aMatrix;
-        Float[] bMatrix;
-        Float[] pi;
+        float[] aList;
+        float[] bList;
+        float[] pi;
+        float[][] AMatrix;
+        float[][] BMatrix;
+        float[][] piMatrix;
+
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Printing the file passed in:");
@@ -51,19 +65,30 @@ public class Main {
         bMatrixStr = sc.nextLine().split(" ");
         piStr = sc.nextLine().split(" ");
 
-        aMatrix = new Float[aMatrixStr.length];
-        bMatrix = new Float[bMatrixStr.length];
-        pi = new Float[piStr.length];
+        aList = new float[aMatrixStr.length];
+        bList = new float[bMatrixStr.length];
+        pi = new float[piStr.length];
 
-        for(int i = 0; i < aMatrix.length; i ++){
-            aMatrix[i] = Float.parseFloat(aMatrixStr[i]);
+        for(int i = 0; i < aList.length; i ++){
+            aList[i] = Float.parseFloat(aMatrixStr[i]);
         }
-        for(int i = 0; i < bMatrix.length; i ++){
-            bMatrix[i] = Float.parseFloat(bMatrixStr[i]);
+        for(int i = 0; i < bList.length; i ++){
+            bList[i] = Float.parseFloat(bMatrixStr[i]);
         }
 
         for(int i = 0; i < pi.length; i ++){
             pi[i] = Float.parseFloat(piStr[i]);
+        }
+
+        AMatrix=createMatrix(aList);
+        BMatrix=createMatrix(bList);
+        piMatrix=createMatrix(pi);
+
+        float[] firstTransition = multiplication(piMatrix, AMatrix);
+        float[] lastTransistion = multiplication(createMatrix(firstTransition),BMatrix);
+
+        for(float i:lastTransistion){
+            System.out.println(i);
         }
 
 
