2c4767f78052354635bc6c4f6f439a4057b85430
LaWox <woxler.platon@gmail.com>
2019-09-04 13:42:23 +0200

    readIn

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
+        String[] aMatrix;
+        String[] bMatrix;
+        String[] pi;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Printing the file passed in:");
-        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+
+        aMatrix = sc.nextLine().split(" ");
+        bMatrix = sc.nextLine().split(" ");
+        pi = sc.nextLine().split(" ");
+
+        
+
     }
 }
