1a5e8b7fe8726ab156c38592aed90073a88f0fa6
LaWox <woxler.platon@gmail.com>
2019-09-04 15:25:12 +0200

    allt e floats

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Main {
 
+    /*
     static Float[] multiplication(String[] a, String[] b,){
         int aRow = Integer.parseInt(a[0]);
         int aCol = Integer.parseInt(a[1]);
@@ -17,17 +18,37 @@ public class Main {
 
         }
     }
+    */
     public static void main(String[] args) {
-        String[] aMatrix;
-        String[] bMatrix;
-        String[] pi;
+        String[] aMatrixStr;
+        String[] bMatrixStr;
+        String[] piStr;
+
+        Float[] aMatrix;
+        Float[] bMatrix;
+        Float[] pi;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Printing the file passed in:");
 
-        aMatrix = sc.nextLine().split(" ");
-        bMatrix = sc.nextLine().split(" ");
-        pi = sc.nextLine().split(" ");
+        aMatrixStr = sc.nextLine().split(" ");
+        bMatrixStr = sc.nextLine().split(" ");
+        piStr = sc.nextLine().split(" ");
+
+        aMatrix = new Float[aMatrixStr.length];
+        bMatrix = new Float[bMatrixStr.length];
+        pi = new Float[piStr.length];
+
+        for(int i = 0; i < aMatrix.length; i ++){
+            aMatrix[i] = Float.parseFloat(aMatrixStr[i]);
+        }
+        for(int i = 0; i < bMatrix.length; i ++){
+            bMatrix[i] = Float.parseFloat(bMatrixStr[i]);
+        }
+
+        for(int i = 0; i < pi.length; i ++){
+            pi[i] = Float.parseFloat(piStr[i]);
+        }
 
 
 
