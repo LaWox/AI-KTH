18da2996797bc41159bf838bbc93a3d05f8ac566
LaWox <woxler.platon@gmail.com>
2019-09-11 10:19:31 +0200

    printMatrix

--- src/Main1.java
+++ src/Main1.java
@@ -68,6 +68,14 @@ public class Main1 {
         return outCol;
     }
 
+    static void printMatrix(float[][] m){
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j < m[0].length; j++)
+                System.out.print(m[i][j] + " ")
+        }
+        System.out.println();
+    }
+
     static double sumMatrix(double[][] matrix){
         double sum = 0;
         for(int i = 0; i < matrix.length; i++){
