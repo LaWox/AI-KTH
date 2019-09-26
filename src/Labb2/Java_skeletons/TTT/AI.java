3d697c2c35f03102537e6cfd423652fd8417b0bb
LaWox <woxler.platon@gmail.com>
2019-09-26 16:53:57 +0200

    tors em

--- src/Labb2/Java_skeletons/TTT/AI.java
+++ src/Labb2/Java_skeletons/TTT/AI.java
@@ -2,36 +2,46 @@ import java.util.*;
 import java.lang.Math;
 
 public class AI{
-    public int depth;
+    public static int depth = 3;
 
     // return the "best" next gameState
-    public GameState getBestMove(Vector<GameState> states, Player player){
+    public static GameState getBestMove(Vector<GameState> states){
+        int index = 0;
+        double max = 0;
+        double v = 0;
 
-        return null;
+        // find state with max value and save its index
+        for(int i = 0; i < states.size(); i++){
+            v = minMaxPruning(states.get(i), depth, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+            if(max < v){
+                max = v;
+                index = i;
+            }
+        }
+        return states.get(index);
     }
 
     // minMax-alg with pruning, recursive
-    private double minMaxPruning(GameState state, int depth, double alpha, double beta){
+    private static double minMaxPruning(GameState state, int depth, double alpha, double beta){
 
-        double v=0;
+        double v = 0;
 
         // A vector containing all possible children states if this current state
         Vector<GameState> nextStates = new Vector<GameState>();
         state.findPossibleMoves(nextStates);
 
         if (depth==0 || nextStates.size()==0){
-            v= evalState(state);
+            v = evalState(state);
         }
-
 
         // 2 is 'O' so if nextPlayer is 2 it means that the current player is 'X'
         else if (state.getNextPlayer() == 2){
             v=Double.NEGATIVE_INFINITY;
 
             for(GameState s: nextStates){
-                v= Math.max(v,minMaxPruning(s,depth-1, alpha, beta));
+                v = Math.max(v, minMaxPruning(s,depth-1, alpha, beta));
                 alpha = Math.max(alpha, v);
-                if( beta <= alpha ) {
+                if(beta <= alpha) {
                     break;
                 }
             }
@@ -41,22 +51,61 @@ public class AI{
             v=Double.POSITIVE_INFINITY;
 
             for(GameState s: nextStates){
-                v= Math.max(v,minMaxPruning(s,depth-1, alpha, beta));
+                v= Math.min(v,minMaxPruning(s,depth-1, alpha, beta));
                 beta = Math.min(beta, v);
                 if( beta <= alpha ) {
                     break;
                 }
             }
         }
-
         return v;
     }
 
     // eval a state and return its value
-    private int evalState(GameState gameState){
+    private static int evalState(GameState state){
+        Move m = state.getMove();
 
+        if(m.isXWin()){
+            return 100;
+        }
+        else if(m.isOWin()){
+            return -1;
+        }
+        int player;
+        Vector<Integer> points = new Vector<Integer>();
+        points.setSize(state.BOARD_SIZE*2+2);
+        Collections.fill(points, 1);
+        int scaling = 3;
+        int value = 0;
 
-        return 0;
+        // where there is an enemy pllayer in the way we want to set the value to zero
+        Vector<Integer> obstructions = new Vector<Integer>();
+
+        for(int row = 0; row < state.BOARD_SIZE; row ++){
+            for(int col = 0; col < state.BOARD_SIZE; col ++){
+                player = state.at(row, col);
+
+                // add points to the vector, increase by a factor for every new entry
+                if(player == 1){
+                    points.set(row, points.get(row)*scaling);
+                    points.add(state.BOARD_SIZE - 1 + col, points.get(state.BOARD_SIZE - 1 + col)*scaling);
+                }
+
+                // if an enemy player is encoutered null the value of the row/col
+                else if(player == 2){
+                    points.set(row, -100);
+                    points.set(state.BOARD_SIZE - 1 + col, -100);
+                }
+            }
+        }
+
+        // if we have nulled the point we don't count it for the value of the state
+        for(Integer i: points){
+            if(i > 0){
+                value += i;
+            }
+        }
+        return value;
     }
 
 }
--- src/Labb2/Java_skeletons/TTT/Player.java
+++ src/Labb2/Java_skeletons/TTT/Player.java
@@ -23,7 +23,11 @@ public class Player {
          * Here you should write your algorithms to get the best next move, i.e.
          * the best next state. This skeleton returns a random move instead.
          */
+
+        return AI.getBestMove(nextStates);
+        /**
         Random random = new Random();
         return nextStates.elementAt(random.nextInt(nextStates.size()));
-    }    
+         */
+    }
 }
